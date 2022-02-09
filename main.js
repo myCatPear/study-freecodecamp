a90b7c64616b6ea0f672c590987cc815493e1658
Denis Tavabilov <dagger1233@gmail.com>
2022-02-09 15:55:27 +0500

    some task

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-let a = 11;
-let b = 9;
-let c = 3;
+let a = 5;
+let b = 12;
+let c = 4.6;
 
 // Only change code below this line
-a -= 6;
-b -= 15;
-c -= 1;
+a *= 5;
+b *= b;
+c *= 10;
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
