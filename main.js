0de5ad60e2970a8e1cc37bc85af094022ca165d9
Denis Tavabilov <dagger1233@gmail.com>
2022-02-09 16:01:43 +0500

    some task new

--- main.js
+++ main.js
@@ -10,3 +10,12 @@ c *= 10;
 const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
 
 const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+const myStr = "This is the start." + " " +  "This is the end."; // Change this line
+
+const ourName = "freeCodeCamp";
+const ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+const anAdjective = "awesome!";
+let ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
