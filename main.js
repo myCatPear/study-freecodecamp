9fad624c979f9c19b6b33a1db4f60cad395c1a7c
Denis Tavabilov <dagger1233@gmail.com>
2022-02-09 16:09:51 +0500

    yami yami

--- main.js
+++ main.js
@@ -1,21 +1,16 @@
-let a = 5;
-let b = 12;
-let c = 4.6;
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
 
 // Only change code below this line
-a *= 5;
-b *= b;
-c *= 10;
+lastNameLength = lastName;
 
-const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
 
-const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+// Only change code below this line
+firstLetterOfLastName = lastName; // Change this line
 
-const myStr = "This is the start." + " " +  "This is the end."; // Change this line
-
-const ourName = "freeCodeCamp";
-const ourStr = "Hello, our name is " + ourName + ", how are you?";
-
-const anAdjective = "awesome!";
-let ourStr = "freeCodeCamp is ";
-ourStr += anAdjective;
+const firstName = "Ada";
+const lastLetter = firstName[firstName.length - 1];
