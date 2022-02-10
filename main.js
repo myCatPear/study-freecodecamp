2adc6afb9c7531fbcb2c903a4983aae8e86c8b15
Denis Tavabilov <dagger1233@gmail.com>
2022-02-10 20:35:32 +0500

    recursion

--- main.js
+++ main.js
@@ -518,3 +518,14 @@ if (i == 10) myArray.push(i);
 i +=1;
 console.log(i)
 } while (i != 11); 
+
+function sum(arr, n) {
+  // Only change code below this line
+if (n <= 0) {
+  return 0;
+} else {
+  return sum(arr, n -1) + arr[n-1];
+}
+  
+  // Only change code above this line
+}
