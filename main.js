cd131fd314699575c5663d8339065656f4d2c1d0
Denis Tavabilov <dagger1233@gmail.com>
2022-02-10 20:10:09 +0500

    for

--- main.js
+++ main.js
@@ -459,3 +459,37 @@ while ( i > -1) {
   myArray.push(i);
   i--;
 }
+
+// Setup
+const myArray = [];
+
+// Only change code below this line
+
+for (let i = 1; i <= 5; i +=1) {
+  myArray.push(i)
+}
+
+
+const ourArray = [];
+
+for (let i = 10; i > 0; i -= 2) {
+  ourArray.push(i);
+}
+
+// Setup
+const myArray = [];
+
+// Only change code below this line
+
+for (let i = 9; i > 0; i -= 2) {
+  myArray.push(i);
+}
+
+// Setup
+const myArr = [2, 3, 4, 5, 6];
+
+// Only change code below this line
+let total = 0;
+for (let i = 0; i <= myArr.length-1; i++) {
+   total += myArr[i];
+}
