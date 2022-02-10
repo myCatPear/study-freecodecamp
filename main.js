286f2445eeedb2d912e371e13d8a52e9e8c85190
Denis Tavabilov <dagger1233@gmail.com>
2022-02-10 20:02:12 +0500

    while

--- main.js
+++ main.js
@@ -414,3 +414,48 @@ const myPlants = [
 ];
 
 const secondTree = myPlants[1].list[1];
+
+// Setup
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+  if (prop != 'tracks' && value !== '') records[id][prop] = value;  
+  else if (prop == 'tracks' && !records[id].hasOwnProperty('tracks')) records[id].tracks = [value];
+  else if (prop == 'tracks' && value != '') records[id].tracks.push(value);
+  else delete records[id][prop];
+
+  return records;
+
+
+}
+
+updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+
+// Setup
+const myArray = [];
+
+// Only change code below this line
+let i = 5;
+while ( i > -1) {
+  myArray.push(i);
+  i--;
+}
