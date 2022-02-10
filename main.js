33a4458b20e56d2c5a3e40624fbf96062651e704
Denis Tavabilov <dagger1233@gmail.com>
2022-02-10 17:41:39 +0500

    case switch

--- main.js
+++ main.js
@@ -58,3 +58,75 @@ function testElse(val) {
 }
 
 testElse(4);
+// 
+
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+if (strokes == 1) return "Hole-in-one!";
+else if (strokes <= par - 2	) return "Eagle";
+else if (strokes == par - 1 ) return "Birdie";
+else if (strokes == par  ) return "Par";
+else if (strokes == par + 1) return "Bogey";
+else if (strokes == par + 2 ) return "Double Bogey";
+else if (strokes >= par + 3) return "Go Home!";
+
+
+  return "Change Me";
+  // Only change code above this line
+}
+
+golfScore(5, 4);
+
+// 
+
+function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+switch(val) {
+  case 1: 
+    answer = 'alpha';
+    break;
+  case 2: 
+    answer = 'beta';
+    break;
+  case 3:
+    answer = 'gamma'
+    break;
+  case 4: 
+  answer = 'delta'
+  break;
+}
+  // Only change code above this line
+  return answer;
+}
+
+caseInSwitch(1);
+
+//
+
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+
+switch (val) {
+  case 'a':
+    answer = 'apple';
+    break;
+  case 'b':
+    answer = 'bird';
+    break;
+  case 'c':
+    answer = 'cat';
+    break;
+    default:
+    answer = 'stuff';
+}
+
+  // Only change code above this line
+  return answer;
+}
+
+switchOfStuff(1);
+//
