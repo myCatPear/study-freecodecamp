f427c9b9b377b58bc3b9c095ee6ac5cd139ceea0
Denis Tavabilov <dagger1233@gmail.com>
2022-02-11 15:48:27 +0500

    reg exp true false

--- main.js
+++ main.js
@@ -534,3 +534,6 @@ let myString = "Hello, World!";
 let myRegex = /Hello/;
 let result = myRegex.test(myString); // Change this line
 
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/; // Change this line
+let result = waldoRegex.test(waldoIsHiding);
