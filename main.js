0c759f91cbf1eeaa456f7f6e5dd3e44a6346ff10
Denis Tavabilov <dagger1233@gmail.com>
2022-02-11 16:01:12 +0500

    reg exp gi

--- main.js
+++ main.js
@@ -537,3 +537,19 @@ let result = myRegex.test(myString); // Change this line
 let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
 let waldoRegex = /Waldo/; // Change this line
 let result = waldoRegex.test(waldoIsHiding);
+
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let result = petRegex.test(petString);
+
+let myString = "freeCodeCamp";
+let fccRegex = /freeCodeCamp/i; // Change this line
+let result = fccRegex.test(myString);
+
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/; // Change this line
+let result = extractStr.match(codingRegex); // Change this line
+
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /twinkle/ig; // Change this line
+let result = twinkleStar.match(starRegex); // Change this line
