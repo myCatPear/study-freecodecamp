eea4d197bc140ede460f613a61a4fdff7a7bcd41
Denis Tavabilov <dagger1233@gmail.com>
2022-02-11 16:40:54 +0500

    reg exp[]//

--- main.js
+++ main.js
@@ -562,3 +562,12 @@ let quoteSample = "Beware of bugs in the above code; I have only proved it corre
 let vowelRegex = /[aeiou]/ig; // Change this line
 let result = quoteSample.match(vowelRegex); // Change this line
 console.log(result)
+
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi; // Change this line
+let result = quoteSample.match(alphabetRegex); // Change this line
+console.log(result)
+
+let quoteSample = "Blueberry 3.141592653s are delicious.";
+let myRegex = /[h-s2-6]/ig; // Change this line
+let result = quoteSample.match(myRegex); // Change this line
